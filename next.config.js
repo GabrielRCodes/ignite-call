f3b0280a4838f1c96cdb077e10d25ea24822741b
GabrielRCodes <gabrielrcodes@gmail.com>
2023-01-30 12:11:25 -0300

    Atualização setup dia 01

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+/* eslint-disable prettier/prettier */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
