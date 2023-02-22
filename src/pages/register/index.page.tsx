37a1ebec5fd05fc210be4c9c9fde0f12198e6735
GabrielRCodes <gabrielrcodes@gmail.com>
2023-02-22 13:28:12 -0300

    Criando estilização da rota register.

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import { Heading, MultiStep, Text, TextInput, Button } from "@ignite-ui/react";
+import { Container, Form, Header } from "./styles";
+import { ArrowRight } from "phosphor-react"
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">
+          Bem-vindo ao Ignite Call!
+        </Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <Form as="form">
+        <label>
+          <Text size="sm">Nome de usuário</Text>
+          <TextInput prefix="ignite.com/" placeholder="seu-usuario" />
+        </label>
+        <label>
+          <Text size="sm">Nome completo</Text>
+          <TextInput placeholder="Seu nome" />
+        </label>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
