45e383f3bcc1fead12435bdbd4941434d9c6d351
GabrielRCodes <gabrielrcodes@gmail.com>
2023-04-06 11:41:43 -0300

    Atualizando API

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -4,10 +4,14 @@ import NextAuth from "next-auth"
 
 declare module "next-auth" {
   export interface User {
-    id: string;
-    name: string;
-    email: string;
-    username: string;
+    id: string
+    name: string
+    email: string
+    username: string
     avatar_url: string
+  }
+
+  interface Session {
+    user: User
   }
 }
--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,9 +1,19 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable camelcase */
 /* eslint-disable prettier/prettier */
+import { prisma } from "@/src/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession } from "next-auth";
+import { z } from "zod";
 import { buildNextAuthOptions } from "../auth/[...nextauth].api";
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(z.object({
+    weekDay: z.number(),
+    startTimeInMinutes: z.number(),
+    endTimeInMinutes: z.number(), 
+  }))
+})
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
@@ -12,7 +22,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   const session = await unstable_getServerSession(req, res, buildNextAuthOptions(req, res))
 
-  return res.json({
-    session,
-  })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  // O mais correto é usar o createMany, porém como estou utilizando SQLite, não é possível inserir mais de uma informação por vez no banco.
+  // await prisma.userTimeInterval.createMany
+  await Promise.all(intervals.map(interval => {
+    return prisma.userTimeInterval.create({
+      data: {
+        week_day: interval.weekDay,
+        time_start_in_minutes: interval.startTimeInMinutes,
+        time_end_in_minutes: interval.endTimeInMinutes,
+        user_id: session.user?.id,
+      },
+    })
+  }))
+
+  return res.status(201).end()
 }
